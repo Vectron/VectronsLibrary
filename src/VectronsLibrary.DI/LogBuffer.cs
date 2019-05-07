3601b9df110a8bc1db575aa9d281b7a67b61b854
Vectron <thijs_bloebaum@hotmail.com>
2019-05-07 11:32:46 +0200

    Make logbuffer public, add ignore attributes to IBufferItem

--- src/VectronsLibrary.DI/LogBuffer.cs
+++ src/VectronsLibrary.DI/LogBuffer.cs
@@ -6,24 +6,21 @@ using System.Collections.Generic;
 namespace VectronsLibrary.DI
 {
     [Ignore]
-    internal class LogBuffer : ILogger
+    public class LogBuffer : ILogger
     {
-        private Queue<IBufferItem> bufferItems = new Queue<IBufferItem>();
+        private readonly Queue<IBufferItem> bufferItems = new Queue<IBufferItem>();
 
+        [Ignore]
         private interface IBufferItem
         {
             void Log(ILogger logger);
         }
 
         public IDisposable BeginScope<TState>(TState state)
-        {
-            return NullScope.Instance;
-        }
+            => NullScope.Instance;
 
         public bool IsEnabled(LogLevel logLevel)
-        {
-            return true;
-        }
+            => true;
 
         public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
             => bufferItems.Enqueue(new BufferItem<TState>(logLevel, eventId, state, exception, formatter));
