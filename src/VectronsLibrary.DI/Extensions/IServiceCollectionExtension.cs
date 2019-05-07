a158ca810a84cbf9273ca330fe77f87f5c9b41f8
Vectron <thijs_bloebaum@hotmail.com>
2019-05-07 13:27:25 +0200

    Refactoring, code cleanup

--- src/VectronsLibrary.DI/Extensions/IServiceCollectionExtension.cs
+++ src/VectronsLibrary.DI/Extensions/IServiceCollectionExtension.cs
@@ -10,10 +10,35 @@ namespace VectronsLibrary.DI
 {
     public static class IServiceCollectionExtension
     {
+        private const string AddMessage = "Adding contract type: {0}, with implementation type: {1} as {3}";
+        private const string GenericAddMessage = "Adding contract type: {0}, with generic implementation type: {1} as {3}";
+        private const string IgnoreMessage = "Ignoring contract type: {0}, with implementation type: {1}";
         private static ILogger logger = NullLogger.Instance;
 
+        public static IServiceCollection Add(this IServiceCollection serviceDescriptors, Type implementation, Type contractType, ServiceLifetime serviceLifetime)
+        {
+            if (implementation.IsGenericTypeDefinition)
+            {
+                logger.LogDebug(GenericAddMessage, contractType.FullName, implementation.FullName, serviceLifetime);
+                serviceDescriptors.Add(ServiceDescriptor.Describe(contractType, implementation, serviceLifetime));
+                return serviceDescriptors;
+            }
+
+            logger.LogDebug(AddMessage, contractType.FullName, implementation.FullName, serviceLifetime);
+
+            if (contractType == implementation)
+            {
+                serviceDescriptors.Add(ServiceDescriptor.Describe(contractType, implementation, serviceLifetime));
+                return serviceDescriptors;
+            }
+
+            serviceDescriptors.Add(ServiceDescriptor.Describe(contractType, x => x.GetService(implementation), serviceLifetime));
+            serviceDescriptors.Add(ServiceDescriptor.Describe(implementation, implementation, serviceLifetime));
+            return serviceDescriptors;
+        }
+
         public static IServiceCollection AddAssemblyResolver(this IServiceCollection serviceDescriptors)
-            => serviceDescriptors
+                    => serviceDescriptors
                 .TryAddByAttribute(typeof(AssemblyResolver), typeof(IAssemblyResolver));
 
         public static IServiceCollection AddByAttribute(this IServiceCollection serviceDescriptors, Type implementation, Type contractType)
@@ -21,85 +46,21 @@ namespace VectronsLibrary.DI
             if (Attribute.IsDefined(implementation, typeof(SingletonAttribute)) ||
                 Attribute.IsDefined(contractType, typeof(SingletonAttribute)))
             {
-                if (implementation.IsGenericTypeDefinition)
-                {
-                    logger.LogDebug("Adding contract type: {0}, with generic implementation type: {1} as singleton",
-                        contractType.FullName,
-                        implementation.FullName);
-                    return serviceDescriptors.AddSingleton(contractType, implementation);
-                }
-
-                if (contractType == implementation)
-                {
-                    logger.LogDebug("Adding contract type: {0}, with implementation type: {1} as singleton",
-                            contractType.FullName,
-                            implementation.FullName);
-                    return serviceDescriptors.AddSingleton(contractType, implementation);
-                }
-
-                logger.LogDebug("Adding contract type: {0}, with implementation type: {1} as singleton",
-                        contractType.FullName,
-                        implementation.FullName);
-                return serviceDescriptors.AddSingleton(contractType, x => x.GetService(implementation))
-                    .AddSingleton(implementation);
+                return serviceDescriptors.Add(implementation, contractType, ServiceLifetime.Singleton);
             }
             else if (Attribute.IsDefined(implementation, typeof(TransientAttribute)) ||
                      Attribute.IsDefined(contractType, typeof(TransientAttribute)))
             {
-                if (implementation.IsGenericTypeDefinition)
-                {
-                    logger.LogDebug("Adding contract type: {0}, with generic implementation type: {1} as transient",
-                           contractType.FullName,
-                           implementation.FullName);
-                    return serviceDescriptors.AddTransient(contractType, implementation);
-                }
-
-                if (contractType == implementation)
-                {
-                    logger.LogDebug("Adding contract type: {0}, with implementation type: {1} as transient",
-                            contractType.FullName,
-                            implementation.FullName);
-                    return serviceDescriptors.AddTransient(contractType, implementation);
-                }
-
-                logger.LogDebug("Adding contract type: {0}, with implementation type: {1} as transient",
-                        contractType.FullName,
-                        implementation.FullName);
-                return serviceDescriptors.AddTransient(contractType, x => x.GetService(implementation))
-                    .AddTransient(implementation);
+                return serviceDescriptors.Add(implementation, contractType, ServiceLifetime.Transient);
             }
             else if (Attribute.IsDefined(implementation, typeof(IgnoreAttribute)) ||
                      Attribute.IsDefined(contractType, typeof(IgnoreAttribute)))
             {
-                logger.LogDebug("Ignoring contract type: {0}, with implementation type: {1} as scoped",
-                       contractType.FullName,
-                       implementation.FullName);
+                logger.LogDebug(IgnoreMessage, contractType.FullName, implementation.FullName);
                 return serviceDescriptors;
             }
-            else
-            {
-                if (implementation.IsGenericTypeDefinition)
-                {
-                    logger.LogDebug("Adding contract type: {0}, with generic implementation type: {1} as scoped",
-                           contractType.FullName,
-                           implementation.FullName);
-                    return serviceDescriptors.AddScoped(contractType, implementation);
-                }
 
-                if (contractType == implementation)
-                {
-                    logger.LogDebug("Adding contract type: {0}, with implementation type: {1} as scoped",
-                            contractType.FullName,
-                            implementation.FullName);
-                    return serviceDescriptors.AddScoped(contractType, implementation);
-                }
-
-                logger.LogDebug("Adding contract type: {0}, with implementation type: {1} as scoped",
-                        contractType.FullName,
-                        implementation.FullName);
-                return serviceDescriptors.AddScoped(contractType, x => x.GetService(implementation))
-                    .AddScoped(implementation);
-            }
+            return serviceDescriptors.Add(implementation, contractType, ServiceLifetime.Scoped);
         }
 
         public static IServiceCollection AddByAttribute(this IServiceCollection serviceDescriptors, IEnumerable<Type> implementations, Type contractType)
@@ -108,7 +69,7 @@ namespace VectronsLibrary.DI
             foreach (var implementation in implementations)
             {
                 noImplementations = false;
-                serviceDescriptors.AddByAttribute(implementation, contractType);
+                _ = serviceDescriptors.AddByAttribute(implementation, contractType);
             }
 
             if (noImplementations)
@@ -130,7 +91,7 @@ namespace VectronsLibrary.DI
             foreach (var @interface in interfaces)
             {
                 var implementations = loadedTypes.GetImplementations(@interface);
-                serviceDescriptors.AddByAttribute(implementations, @interface);
+                _ = serviceDescriptors.AddByAttribute(implementations, @interface);
             }
 
             return serviceDescriptors;
@@ -151,82 +112,46 @@ namespace VectronsLibrary.DI
             return serviceDescriptors;
         }
 
+        public static IServiceCollection TryAdd(this IServiceCollection serviceDescriptors, Type implementation, Type contractType, ServiceLifetime serviceLifetime)
+        {
+            if (implementation.IsGenericTypeDefinition)
+            {
+                logger.LogDebug(GenericAddMessage, contractType.FullName, implementation.FullName, serviceLifetime);
+                return serviceDescriptors.TryAdd(ServiceDescriptor.Describe(contractType, implementation, serviceLifetime));
+            }
+
+            logger.LogDebug(AddMessage, contractType.FullName, implementation.FullName, serviceLifetime);
+
+            if (contractType == implementation)
+            {
+                return serviceDescriptors.TryAdd(ServiceDescriptor.Describe(contractType, implementation, serviceLifetime));
+            }
+
+            return serviceDescriptors
+                .TryAdd(ServiceDescriptor.Describe(contractType, x => x.GetService(implementation), serviceLifetime))
+                .TryAdd(ServiceDescriptor.Describe(implementation, implementation, serviceLifetime));
+        }
+
         public static IServiceCollection TryAddByAttribute(this IServiceCollection serviceDescriptors, Type implementation, Type contractType)
         {
             if (Attribute.IsDefined(implementation, typeof(SingletonAttribute)) ||
                 Attribute.IsDefined(contractType, typeof(SingletonAttribute)))
             {
-                if (implementation.IsGenericTypeDefinition)
-                {
-                    logger.LogDebug("Try adding contract type: {0}, with generic implementation type: {1} as singleton",
-                        contractType.FullName,
-                        implementation.FullName);
-                    return serviceDescriptors.TryAddSingleton(contractType, implementation);
-                }
-
-                if (contractType == implementation)
-                {
-                    logger.LogDebug("Try Adding contract type: {0}, with implementation type: {1} as singleton",
-                            contractType.FullName,
-                            implementation.FullName);
-                    return serviceDescriptors.TryAddSingleton(contractType, implementation);
-                }
-
-                logger.LogDebug("Try adding contract type: {0}, with implementation type: {1} as singleton",
-                        contractType.FullName,
-                        implementation.FullName);
-                return serviceDescriptors.TryAddSingleton(contractType, x => x.GetService(implementation))
-                    .TryAddSingleton(implementation);
+                return serviceDescriptors.TryAdd(implementation, contractType, ServiceLifetime.Singleton);
             }
             else if (Attribute.IsDefined(implementation, typeof(TransientAttribute)) ||
                      Attribute.IsDefined(contractType, typeof(TransientAttribute)))
             {
-                if (implementation.IsGenericTypeDefinition)
-                {
-                    logger.LogDebug("Try adding contract type: {0}, with generic implementation type: {1} as transient",
-                           contractType.FullName,
-                           implementation.FullName);
-                    return serviceDescriptors.TryAddTransient(contractType, implementation);
-                }
-
-                if (contractType == implementation)
-                {
-                    logger.LogDebug("Try Adding contract type: {0}, with implementation type: {1} as transient",
-                            contractType.FullName,
-                            implementation.FullName);
-                    return serviceDescriptors.TryAddTransient(contractType, implementation);
-                }
-
-                logger.LogDebug("Try adding contract type: {0}, with implementation type: {1} as transient",
-                        contractType.FullName,
-                        implementation.FullName);
-                return serviceDescriptors.TryAddTransient(contractType, x => x.GetService(implementation))
-                    .TryAddTransient(implementation);
+                return serviceDescriptors.TryAdd(implementation, contractType, ServiceLifetime.Transient);
             }
-            else
+            else if (Attribute.IsDefined(implementation, typeof(IgnoreAttribute)) ||
+                     Attribute.IsDefined(contractType, typeof(IgnoreAttribute)))
             {
-                if (implementation.IsGenericTypeDefinition)
-                {
-                    logger.LogDebug("Try adding contract type: {0}, with generic implementation type: {1} as scoped",
-                           contractType.FullName,
-                           implementation.FullName);
-                    return serviceDescriptors.TryAddScoped(contractType, implementation);
-                }
-
-                if (contractType == implementation)
-                {
-                    logger.LogDebug("Try Adding contract type: {0}, with implementation type: {1} as scoped",
-                            contractType.FullName,
-                            implementation.FullName);
-                    return serviceDescriptors.TryAddScoped(contractType, implementation);
-                }
-
-                logger.LogDebug("Try adding contract type: {0}, with implementation type: {1} as scoped",
-                        contractType.FullName,
-                        implementation.FullName);
-                return serviceDescriptors.TryAddScoped(contractType, x => x.GetService(implementation))
-                    .TryAddScoped(implementation);
+                logger.LogDebug(IgnoreMessage, contractType.FullName, implementation.FullName);
+                return serviceDescriptors;
             }
+
+            return serviceDescriptors.TryAdd(implementation, contractType, ServiceLifetime.Scoped);
         }
     }
 }
