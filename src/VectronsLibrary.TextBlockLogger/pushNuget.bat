a7a47af8d98f32df6e0a370a3af3aec337c530b9
Vectron <thijs_bloebaum@hotmail.com>
2020-02-26 12:53:13 +0100

    Switch over projects to new project style Add support for .net core

--- src/VectronsLibrary.TextBlockLogger/pushNuget.bat
+++
@@ -1,3 +0,0 @@
-nuget pack VectronsLibrary.TextBlockLogger.csproj -Build -Symbols -Properties Configuration=Release
-dotnet nuget push VectronsLibrary.TextBlockLogger.1.0.0.nupkg -k oy2d6c7k6co2p67cozdqhyx4epcrhmduxoxnwgtd3kcw7m -s https://api.nuget.org/v3/index.json
-pause
