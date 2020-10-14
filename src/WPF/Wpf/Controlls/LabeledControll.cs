595d5b0da243cca694221bf9b1ab24491c935bec
Vectron <thijs_bloebaum@hotmail.com>
2020-10-14 20:53:39 +0200

    [VectronsLibrary.Wpf] Replace the labeled controlls with a labeled container

---
+++ src/WPF/Wpf/Controlls/LabeledControll.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace VectronsLibrary.Wpf.Controlls
+{
+    public class LabeledControll : ContentControl
+    {
+        public static readonly DependencyProperty LabelProperty = DependencyProperty
+            .Register(nameof(Label), typeof(string), typeof(LabeledControll), new FrameworkPropertyMetadata("Unnamed Label"));
+
+        static LabeledControll()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(LabeledControll), new FrameworkPropertyMetadata(typeof(LabeledControll)));
+        }
+
+        public string Label
+        {
+            get => (string)GetValue(LabelProperty);
+            set => SetValue(LabelProperty, value);
+        }
+    }
+}
