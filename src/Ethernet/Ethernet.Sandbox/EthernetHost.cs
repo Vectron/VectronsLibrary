57632f5d98e64ecb32d9494c1ad7ee25fc4e59df
Vectron <thijs_bloebaum@hotmail.com>
2022-07-26 20:15:38 +0200

    [Ethernet] Add a sandbox for quick ethernet testing

---
+++ src/Ethernet/Ethernet.Sandbox/EthernetHost.cs
@@ -0,0 +1,47 @@
+﻿using System.Net.Sockets;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace VectronsLibrary.Ethernet.Sandbox;
+
+/// <summary>
+/// A <see cref="BackgroundService"/> for opening an ethernet server.
+/// </summary>
+internal class EthernetHost : BackgroundService
+{
+    private readonly IEthernetServer ethernetServer;
+    private readonly ILogger<EthernetHost> logger;
+    private IDisposable? sessionStream;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="EthernetHost"/> class.
+    /// </summary>
+    /// <param name="logger">A <see cref="ILogger"/>.</param>
+    /// <param name="ethernetServer">A <see cref="IEthernetServer"/>.</param>
+    public EthernetHost(ILogger<EthernetHost> logger, IEthernetServer ethernetServer)
+    {
+        this.logger = logger;
+        this.ethernetServer = ethernetServer;
+    }
+
+    /// <inheritdoc/>
+    public override void Dispose()
+    {
+        sessionStream?.Dispose();
+        base.Dispose();
+    }
+
+    /// <inheritdoc/>
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        ethernetServer.Open("127.0.0.1", 200, ProtocolType.Tcp);
+        sessionStream = ethernetServer.SessionStream.Subscribe(async x => await OnClient(x));
+        return Task.CompletedTask;
+    }
+
+    private async Task OnClient(IConnected<IEthernetConnection> obj)
+    {
+        await Task.Delay(5000);
+        logger.LogInformation("Client state changed {IsConnected}, {Socket}", obj.IsConnected, obj.Value?.Socket);
+    }
+}
---
+++ src/Ethernet/Ethernet.Sandbox/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace VectronsLibrary.Ethernet.Sandbox;
+
+/// <summary>
+/// Main entry point.
+/// </summary>
+internal class Program
+{
+    private static void ConfigureLogging(HostBuilderContext context, ILoggingBuilder builder)
+        => _ = builder.AddSimpleConsole(options =>
+        {
+            options.IncludeScopes = true;
+            options.SingleLine = true;
+            options.TimestampFormat = "hh:mm:ss:fff ";
+        });
+
+    private static void ConfigureServices(HostBuilderContext context, IServiceCollection services)
+    {
+        _ = services.AddScoped<IEthernetServer, EthernetServer>();
+        _ = services.AddHostedService<EthernetHost>();
+    }
+
+    private static Task Main(string[] args)
+        => Host.CreateDefaultBuilder(args)
+            .ConfigureLogging(ConfigureLogging)
+            .ConfigureServices(ConfigureServices)
+            .RunConsoleAsync(CancellationToken.None);
+}
