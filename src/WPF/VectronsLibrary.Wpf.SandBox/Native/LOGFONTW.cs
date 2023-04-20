29e1a0ea6cd646bf03f9725a5a2ab82b99ccb514
Vectron <thijs_bloebaum@hotmail.com>
2023-04-20 13:09:27 +0200

    Add a WPF sandbox

---
+++ src/WPF/VectronsLibrary.Wpf.SandBox/Native/LOGFONTW.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace Windows.Win32.Graphics.Gdi;
+
+/// <summary>Defines the attributes of a font.</summary>
+/// <remarks>
+/// <para>The following situations do not support ClearType antialiasing: </para>
+/// <para>This doc was truncated.</para>
+/// <para><see href="https://docs.microsoft.com/windows/win32/api//dimm/ns-dimm-logfontw#">Read more on docs.microsoft.com</see>.</para>
+/// </remarks>
+internal partial struct LOGFONTW
+{
+    public static LOGFONTW FromFont(FontFamily fontFamily, FontStretch fontStretch, FontStyle fontStyle, FontWeight fontWeight, Color color, double size)
+    {
+        var font = new LOGFONTW
+        {
+            lfCharSet = FONT_CHARSET.ANSI_CHARSET,
+            lfClipPrecision = 0,
+            lfEscapement = 0,
+            lfFaceName = "",
+            lfHeight = 0,
+            lfItalic = 0,
+            lfOrientation = 0,
+            lfOutPrecision = 0,
+            lfPitchAndFamily = 0,
+            lfQuality = 0,
+            lfStrikeOut = 0,
+            lfUnderline = 0,
+            lfWeight = 0,
+            lfWidth = 0,
+        };
+    }
+}
---
+++ src/WPF/VectronsLibrary.Wpf.SandBox/Native/PInvoke.cs
@@ -0,0 +1,8 @@
+﻿namespace Windows.Win32;
+
+/// <summary>
+/// Native function calls.
+/// </summary>
+internal static partial class PInvoke
+{
+}
